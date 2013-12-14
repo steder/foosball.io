8e7206e526f3dd6e048f1b07c531b2b45e5d8b29
Mike Steder <steder@gmail.com>
2013-12-14 17:52:28 -0600

    pages!

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,13 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+    // put your routes here
+    this.route('index', { path: '/' });
+    this.resource('leagues', function() { 
+        this.resource('league', { path: '/leagues/:league_id' });
+    });
+    this.route('account', { path: '/account'});
+    this.route('about', { path: '/about' });
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -9,3 +15,16 @@ App.IndexRoute = Ember.Route.extend({
     return ['red', 'yellow', 'blue'];
   }
 });
+
+/*
+App.ApplicationRoute = Ember.Route.extend({
+  setupController: function(controller) {
+    // `controller` is the instance of ApplicationController
+    controller.set('title', "Hello world!");
+  }
+});
+
+App.ApplicationController = Ember.Controller.extend({
+  appName: 'My First Example'
+});
+*/
