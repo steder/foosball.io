17091e4141b073e5ba9f06aef75e82d3d0ee8300
Mike Steder <steder@gmail.com>
2013-12-17 21:57:52 -0600

    serve the ember app from all urls on the server side aside from /static/ and /api/*

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask import jsonify
 from flask import render_template
-from flask.ext.assets import Environment, Bundle
+from flask.ext.assets import Environment #, Bundle
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -11,9 +12,15 @@ assets = Environment(app)
 #assets.register('js_all', js)
 
 
-@app.route("/")
-def index():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
     return render_template("index.html")
+
+
+@app.route("/api")
+def api_placeholder():
+    return jsonify({"placeholder": "stuff"})
 
 
 if __name__ == "__main__":
