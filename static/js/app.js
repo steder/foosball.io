73b744eac4865a274e09278a5cbe9b398945355d
Mike Steder <steder@gmail.com>
2014-01-01 00:41:16 -0600

    Updating app to use ember fixtures

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,12 @@ App = Ember.Application.create({
      LOG_TRANSITIONS: true
 });
 
-App.Store = DS.Store.extend({
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+/* App.Store = DS.Store.extend({
   revision: 13,
   adapter: DS.FixtureAdapter
-});
+}); */
 
 App.Router.reopen({
     location: 'history'
@@ -36,6 +38,32 @@ App.IndexRoute = Ember.Route.extend({
 });
 
 
+App.League = DS.Model.extend({
+  name: DS.attr( 'string' ),
+  games: DS.hasMany("game", {async: true}),
+  players: DS.hasMany("player", {async: true})
+});
+
+App.League.FIXTURES = [
+  { id: 1, name: 'Skinny Scrub',
+    games: [1, 2],
+    players: [1, 2]
+   },
+  { id: 2, name: 'Skinny Pro'}
+];
+
+
+App.LeaguesIndexRoute = Ember.Route.extend({
+    model: function() {
+        // console: returning all the leagues
+        return this.store.find('league');
+    },
+    setupController: function(controller, model) {
+        controller.set('model', model);
+    }
+})
+
+
 App.LeaguesController = Ember.Controller.extend({
     // initial values of controller state:
     someFlag: false,
@@ -55,28 +83,61 @@ App.LeaguesLeagueController = Ember.Controller.extend({
 
 
 App.LeagueRoute = Ember.Route.extend({
-    model: function(league_id) {
+    model: function(params) {
         console.log("returning a league model...");
-        return {name: 'Skinny Scrub League'};
+        return this.store.find('league', params.league_id);
     }
 });
+
+
+App.Player = DS.Model.extend({
+  name: DS.attr( 'string' ),
+});
+
+App.Player.FIXTURES = [
+  { id: 1, name: 'Somebody'},
+  { id: 2, name: 'Someone Else'}
+];
 
 
 App.PlayerRoute = Ember.Route.extend({
-    model: function(player_id) {
+    model: function(params) {
         console.log("returning a player model...");
-        return {name: 'Somebody!'};
+        return this.store.find('player', params.player_id);
     }
 });
 
 
+App.Game = DS.Model.extend({
+    name: DS.attr('string'),
+    homeScore: DS.attr("number"),
+    visitorScore: DS.attr("number"),
+    league: DS.belongsTo("league"),
+    homePlayers: DS.hasMany("player", {async: true}),
+    visitorPlayers: DS.hasMany("player", {async: true})
+});
+
+App.Game.FIXTURES = [
+        {id: 1, name: 'Game 1', 
+         homeScore: 3,
+         visitorScore: 10,
+         league: 1,
+         homePlayers: [1],
+         visitorPlayers: [2]
+        },
+        {id: 2, name: 'Game 2',
+         homeScore: 10,
+         visitorScore: 3,
+         league: 1,
+         homePlayers: [2],
+         visitorPlayers: [1]
+        }
+];
+
+
 App.GameRoute = Ember.Route.extend({
-    model: function(game_id) {
+    model: function(params) {
         console.log("returning a game model...");
-        return {
-            name: 'Game 1', 
-            homeScore: 3,
-            visitorScore: 10
-        };
+        return this.store.find('game', params.game_id)
     }
 });
