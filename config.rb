5878c245d9669ea57d83f2676b22eeaab1f7f337
Nathan Hinshaw <nathanh@skinnycorp.com>
2014-01-18 12:01:28 -0600

    more compass

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "static/scss"
+images_dir = "static/images"
+javascripts_dir = "static/js"
+
+output_style = :compressed
+line_comments = false
